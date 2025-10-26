41ee971db2bf6dd6a9b0d206e2169e719e80a34f
yasnamousavi210@gmail.com <yasnamousavi210@gmail.com>
2025-10-26 17:47:53 +0330

    add sections pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('panel.index');
+});
+Route::get('/form', function () {
+    return view('panel.form');
 });
