ec3460f2863f008239d6674e464c66a529b4ff0c
hooman898589 <homan8995@gmail.ocm>
2025-11-12 16:24:27 +0330

    add Routes subject

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-    return view('panel.index');
+    return view('welcome');
 });
-Route::get('/form', function () {
-    return view('panel.form');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/subjects', [\App\Http\Controllers\SubjectController::class, 'index'])->name('subject.index');
+Route::get('/subjects/create', [\App\Http\Controllers\SubjectController::class, 'create'])->name('subject.create');
+Route::post('/subjects/create', [\App\Http\Controllers\SubjectController::class, 'store'])->name('subject.store');
+Route::put('/subjects/{slug}', [\App\Http\Controllers\SubjectController::class, 'update'])->name('subject.update');
+Route::delete('/subjects/{slug}', [\App\Http\Controllers\SubjectController::class, 'destroy'])->name('subject.destroy');
+
+require __DIR__.'/auth.php';
