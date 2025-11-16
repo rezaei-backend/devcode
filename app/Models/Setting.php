bb7b314aeaad69b70a602f8a18fad327a6e734bd
Hamid Tafreshinia <hamid.tf2004z@gmail.com>
2025-11-16 18:17:29 +0330

    Create Setting.php
    Create model setting

---
+++ app/Models/Setting.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    protected $table = 'settings';
+
+    protected $fillable = [
+        'site_name',
+        'logo_path',
+        'favicon_path',
+        'meta_description',
+        'default_language',
+        'contact_email',
+        'updated_by',
+    ];
+
+}
