b5b406561bb3075339d6c17c73bcb2b4ef6080a9
hooman898589 <homan8995@gmail.ocm>
2025-11-20 18:05:29 +0330

    add breeze

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Language extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'primary_color',
+        'secondary_color',
+
+
+    ];
+
+
 }
